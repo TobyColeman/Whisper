6e841cd0affc7741351e2bd3eb967f36fbf5bfdc
worzel666 <jack.parsons1995@hotmail.com>
2015-06-29 22:53:20 +0100

    hide icon

--- js/background/tab_switch.js
+++ js/background/tab_switch.js
@@ -6,4 +6,7 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
     if (tab.url.match(facebook_url)) {
         chrome.pageAction.show(tabId);
     }
+    else{
+    	chrome.pageAction.hide(tabId)
+    }
 });
