875a72f37faf9614be7d68a7beb8a4edb83b6152
Toby <tobych82@gmail.com>
2015-07-02 22:50:45 +0100

    openpgp import test

--- js/background/tab_switch.js
+++ js/background/tab_switch.js
@@ -6,6 +6,10 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
 	// if the user is viewing messages show the icon, otherwise hide it
     if (tab.url.match(facebook_url)) {
         chrome.pageAction.show(tabId);
+
+        chrome.tabs.executeScript(tabId, {file:"../lib/openpgp.min.js"}, function(){
+
+        });
     }
     else{
     	chrome.pageAction.hide(tabId);
--- js/content/encrypt.js
+++ js/content/encrypt.js
@@ -11,7 +11,7 @@ encryptContainer.className="_6a _m _2of _9_h";
 
 // encrypt button
 var encryptBtn = document.createElement("A");
-encryptBtn.className = "_214 uiButton ion-unlocked";
+encryptBtn.className = "_214 uiButton ion-unlocked ion-clickable";
 encryptBtn.id = "encrypt-toggle";
 
 // get the chat bar and append the encrypt button to it
@@ -20,5 +20,22 @@ var chatBar = document.getElementsByClassName('_211')[0];
 // inject elements
 encryptContainer.appendChild(encryptBtn);
 chatBar.appendChild(encryptContainer);
+
+var openpgp = window.openpgp;
+
+var options = {
+    numBits: 2048,
+    userId: 'Jon Smith <jon.smith@example.org>',
+    passphrase: 'super long and hard to guess secret'
+};
+
+openpgp.generateKeyPair(options).then(function(keypair) {
+    // success
+    var privkey = keypair.privateKeyArmored;
+    var pubkey = keypair.publicKeyArmored;
+    console.log(privkey);
+}).catch(function(error) {
+    // failure
+});
 
 })();
